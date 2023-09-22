46d2aa55ab951f182a94efc8533b94a204ae5d17
yongkangchen <yongkangchen@Yongkangs-MacBook-Air.local>
2023-09-21 18:52:00 -0700

    update

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ mkdir -p /Users/Shared/bin/
 curl -o /Users/Shared/bin/onguard.sh https://raw.githubusercontent.com/yongkangchen/ReinstallGuard/main/onguard.sh
 chmod +x /Users/Shared/bin/onguard.sh
 curl -o ~/Library/LaunchAgents/onguard.plist https://raw.githubusercontent.com/yongkangchen/ReinstallGuard/main/onguard.plist
-launchctl load ~/Library/LaunchAgents/onguard.plist
+launchctl load ~/Library/LaunchAgents/onguard.plist >> /dev/null 2>&1
