8bb41349d62b0cb8eee1846bcadb9d3024d8e71a
yongkangchen <yongkangchen@Yongkangs-MacBook-Air.local>
2023-09-21 18:26:00 -0700

    change bin dir

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-mkdir -p /usr/local/bin
-curl -o /usr/local/bin/onguard.sh https://raw.githubusercontent.com/yongkangchen/ReinstallGuard/main/onguard.sh
-chmod +x /usr/local/bin/onguard.sh
-curl -o ~/Library/LaunchAgents/onguard.plist https://raw.githubusercontent.com/yongkangchen/ReinstallGuard/main/onguard.sh.plist
+mkdir -p ~/bin
+curl -o ~/bin/onguard.sh https://raw.githubusercontent.com/yongkangchen/ReinstallGuard/main/onguard.sh
+chmod +x ~/bin/onguard.sh
+curl -o ~/Library/LaunchAgents/onguard.plist https://raw.githubusercontent.com/yongkangchen/ReinstallGuard/main/onguard.plist
 launchctl load ~/Library/LaunchAgents/onguard.plist
