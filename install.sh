77c83414141a9d248c6f8d5f7857d131f68c7a7c
Yongkang Chen <lx1988cyk@gmail.com>
2023-10-19 19:18:17 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir -p /Users/Shared/bin/
 curl -o /Users/Shared/bin/onguard.sh https://raw.githubusercontent.com/yongkangchen/ReinstallGuard/main/onguard.sh
 chmod +x /Users/Shared/bin/onguard.sh
-curl -o ~/Library/LaunchAgents/onguard.plist https://raw.githubusercontent.com/yongkangchen/ReinstallGuard/main/onguard.plist
-launchctl load ~/Library/LaunchAgents/onguard.plist >> /dev/null 2>&1
+curl -o /Library/LaunchDaemons/onguard.plist https://raw.githubusercontent.com/yongkangchen/ReinstallGuard/main/onguard.plist
+launchctl load /Library/LaunchDaemons/onguard.plist >> /dev/null 2>&1
 /bin/bash /Users/Shared/bin/onguard.sh
